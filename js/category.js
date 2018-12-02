e4abf88fbb2f94da105b200c7648ea4601642d3e
Istiaque Siddiqi <istiaquehaque@gmail.com>
2018-12-02 22:40:49 +0530

    Update category.js

--- js/category.js
+++ js/category.js
@@ -136,7 +136,7 @@ const createGifCard = (gif) => {
         if (navigator.share) {
             // Web Share API is supported
             navigator.share({
-                title: document.title,
+                title: https://gify-pwa.herokuapp.com,
                 url: gif.img,
             }).then(() => console.log('Successful share'))
             .catch((error) => console.log('Error sharing', error));
