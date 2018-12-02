4ace97d3ecf14ff4234da3074017fdb7fdb140aa
Istiaque Siddiqi <istiaquehaque@gmail.com>
2018-12-02 21:48:44 +0530

    Update search.js

--- js/search.js
+++ js/search.js
@@ -137,6 +137,7 @@ const createGifCard = (gif) => {
         if (navigator.share) {
             // Web Share API is supported
             navigator.share({
+                title: document.title,
                 url: gif.img,
             }).then(() => console.log('Successful share'))
             .catch((error) => console.log('Error sharing', error));
